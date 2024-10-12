1b20888506983433a2677e4da979050493062d8a
Tomas <l20220058@ssptaji.cz>
2024-10-12 10:12:38 +0200

    ohm

--- app.js
+++ app.js
@@ -10,3 +10,63 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+function napis(){
+    var hodnota = document.getElementById('vyber').value;
+
+    if (hodnota === 'odpor'){
+        document.getElementById('label1').innerText = 'Napětí:';
+        document.getElementById('label2').innerText = 'Proud:';
+    }
+    else{
+        if (hodnota === 'napeti'){
+            document.getElementById('label1').innerText = 'Odpor:';
+            document.getElementById('label2').innerText = 'Proud:';
+        }
+        else{
+            if (hodnota === 'proud'){
+                document.getElementById('label1').innerText = 'Napětí:';
+                document.getElementById('label2').innerText = 'Odpor:';
+            }
+        }
+    }
+}
+
+function vypocet(){
+    const hodnotaVyberu = document.getElementById('vyber').value;
+    let cislo1 = parseFloat(document.getElementById('cislo1').value, 10);
+    let cislo2 = parseFloat(document.getElementById('cislo2').value, 10);
+    let vysledek;
+
+    if (hodnotaVyberu === 'operace'){
+        vysledek = 'Nemáte vybranou žádnou operaci!';
+        document.getElementById('label1').innerText = '';
+        document.getElementById('label2').innerText = '';
+    }
+    else{
+        if (hodnotaVyberu === 'odpor'){
+            if (cislo2 === 0){
+                vysledek = 'Nulou se nedá dělit!';
+            }
+            else{
+                vysledek = cislo1 / cislo2;
+            }
+        }
+        else{
+            if(hodnotaVyberu === 'proud'){
+                if (cislo2 === 0){
+                   vysledek = 'Nulou se nedá dělit!';
+                }
+                else{
+                    vysledek = cislo1 / cislo2;
+                }
+            }
+            else{
+                if (hodnotaVyberu === 'napeti'){
+                    vysledek = cislo1 * cislo2;
+                }
+            }
+        }
+    }
+
+    document.getElementById('vysledekCislo').innerText = vysledek;
+}
